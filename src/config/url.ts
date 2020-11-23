47f8eefebca9624f1625837f8f6f38ff69057996
Richard Nassar <rnassar@areeba.com>
2020-11-23 19:48:53 +0200

    project deployed on firebase

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,7 +1,7 @@
 /** Employee-ms login route PROD */
-export const EMPLOYEE_MS_LOGIN = 'http://fittech-employee-ms.herokuapp.com/login';
+export const EMPLOYEE_MS_LOGIN = 'https://fittech-employee-ms.herokuapp.com/login';
 // export const EMPLOYEE_MS_LOGIN = 'http://localhost:5000/login';
 
 /** Employee-ms base url PROD  */
-export const EMPLOYEE_MS = 'http://fittech-employee-ms.herokuapp.com';
+export const EMPLOYEE_MS = 'https://fittech-employee-ms.herokuapp.com';
 // export const EMPLOYEE_MS = 'http://localhost:5000';
